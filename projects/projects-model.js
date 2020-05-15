abf43d24a894bdd5f7c0767253acdf5050cb88a4
RobertMisch <robertmisch94@gmail.com>
2020-05-15 12:52:55 -0400

    fixed up getTask

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -17,8 +17,10 @@ function findById(id) {
     .first()
 }
 function findTasks(id){
-    return db("tasks")
-        .where({project_id:id})
+    return db("tasks as t")
+    .join('projects as p','p.id','t.project_id')
+    .select('t.id as task_id','p.name','p.description as project_description','t.description','t.notes')
+        .where({'t.project_id':id})
 }
 function add(project) {
     return db("projects")
